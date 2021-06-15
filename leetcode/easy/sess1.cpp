a60c1d4952f0c3045bced1a55d794265f286efd0
Vortexx <janmayshuklay@gmail.com>
2021-06-15 10:50:41 +0530

    Initial setup

---
+++ leetcode/easy/sess1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+#include <map>
+
+using namespace std;
+
+class Solution
+{
+public:
+  vector<int> twoSum(vector<int> &nums, int target)
+  {
+    vector<int> result(2);
+    map<int, bool> util;
+
+    
+
+  }
+};
