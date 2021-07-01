6a6de3ebf40fa09c58aadaefa79ff1d591953f07
Vortexx <janmayshuklay@gmail.com>
2021-07-01 18:30:38 +0530

    Baseball cpp done

---
+++ leetcode/easy/682-baseball/a.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+bool isNumber(const string &s)
+{
+  auto itr = s.begin();
+  while (itr != s.end())
+  {
+    if (isdigit(*itr))
+      return true;
+
+    itr++;
+  }
+
+  return false;
+}
+
+class Solution
+{
+public:
+  int calPoints(vector<string> &ops)
+  {
+    vector<int> recs;
+    int sum = 0;
+
+    for (auto &s : ops)
+    {
+      int val;
+      if (isNumber(s))
+      {
+        val = stoi(s);
+        recs.push_back(val);
+      }
+      else
+      {
+        if (s == "+")
+        {
+          val = recs[recs.size() - 1] + recs[recs.size() - 2];
+          recs.push_back(val);
+        }
+
+        else if (s == "D")
+        {
+          val = recs[recs.size() - 1] * 2;
+          recs.push_back(val);
+        }
+
+        else if (s == "C")
+        {
+          val = recs[recs.size() - 1] * -1;
+          recs.pop_back();
+        }
+      }
+
+      sum += val;
+    }
+
+    return sum;
+  }
+};
