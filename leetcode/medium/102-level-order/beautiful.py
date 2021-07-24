aa75f15fc1d7817ee48c950a551f9e915a758036
Vortexx <janmayshuklay@gmail.com>
2021-07-24 16:48:14 +0530

    102 beautiful solution done

---
+++ leetcode/medium/102-level-order/beautiful.py
@@ -0,0 +1,43 @@
+"""
+  @author Vortexx2
+  Problem 102 - Binary Tree Level Order Traversal
+
+  Runtime - 32 ms
+  Memory Usage - 14.6 MB
+"""
+from typing import List
+
+
+# Definition for a binary tree node.
+class TreeNode:
+  def __init__(self, val=0, left=None, right=None):
+    self.val = val
+    self.left = left
+    self.right = right
+
+
+class Solution:
+  def levelOrder(self, root: TreeNode) -> List[List[int]]:
+    res = []
+
+    if not root:
+      return res
+
+    level = [root]
+
+    while root and level:
+      currNodes = []
+      nextLevel = []
+
+      for node in level:
+        currNodes.append(node.val)
+        if node.left:
+          nextLevel.append(node.left)
+
+        if node.right:
+          nextLevel.append(node.right)
+
+      res.append(currNodes)
+      level = nextLevel
+
+    return res
