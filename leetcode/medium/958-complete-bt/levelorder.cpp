6cbfc5089358153dc8edc688c0ef6278b9147b69
Vortexx2 <janmayshuklay@gmail.com>
2021-11-09 17:15:25 +0530

    Added metrics and comments to level order (958)

--- leetcode/medium/958-complete-bt/levelorder.cpp
+++ leetcode/medium/958-complete-bt/levelorder.cpp
@@ -1,3 +1,13 @@
+/**
+ * @file levelorder.cpp
+ * @author Vortexx2
+ * @brief Problem 958 - Check Completeness of BT
+ * @approach - Using iterative level order traversal
+ * @date 09-11-2021
+ *
+ * Runtime - 4 ms
+ * Memory Usage - 10.5 MB
+ */
 #include <iostream>
 #include <vector>
 
@@ -22,14 +32,19 @@ class Solution {
     if (!root) return true;
 
     vector<TreeNode *> level = {root};
+    // has to be declared in the most outter scope possible, if it ever turns
+    // true and we encounter another child, we know tree is not balanced
     bool prevNotComplete = false;
 
     while (!level.empty()) {
       vector<TreeNode *> nextLevel;
 
       for (TreeNode *&node : level) {
+        // node has right child but not left child
         if (!node->left && node->right) return false;
 
+        // if any of previous nodes missed a child, but current node has a
+        // child, not complete
         if (prevNotComplete && (node->left || node->right)) return false;
 
         if (node->left)
