1dfe3ec1e08fb970feadbe6f63c0750e228030f0
Vortexx2 <janmayshuklay@gmail.com>
2022-10-12 11:43:10 +0530

    Problem 542 - bfs cpp added comments

--- leetcode/medium/542-01-matrix/bfs.cpp
+++ leetcode/medium/542-01-matrix/bfs.cpp
@@ -1,3 +1,14 @@
+/**
+ * @file bfs.cpp
+ * @author Vortexx2
+ * @brief Problem 542 - 01 Matrix
+ * @approach Using a tactical approach where we slowly expand from the already
+ * known node i.e. the nodes that are 0
+ * @date 12-10-2022
+ *
+ * Runtime - 128 ms O(m * n)
+ * Memory Usage - 32 MB O(m * n)
+ */
 #include <iostream>
 #include <queue>
 #include <stack>
