83b6ffdedb5ed8a68d78ac00dcc1198b13831af4
Vortexx2 <janmayshuklay@gmail.com>
2023-05-27 23:23:59 +0530

    Problem 877 - simplest cpp sol done

---
+++ leetcode/medium/877-stone-game/best.cpp
@@ -0,0 +1,18 @@
+/**
+ * @file best.cpp
+ * @author Vortexx2
+ * @brief Problem 877 - Stone Game
+ * @approach Using logic, best sol
+ * @date 27-05-2023
+ * 
+ * Runtime - 6 ms O(1)
+ * Memory Usage - 7.4 MB O(1)
+ */
+#include<bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+ public:
+  bool stoneGame(vector<int>& piles) { return true; }
+};
