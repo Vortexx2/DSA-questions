af81467d113b0e8568145cb4ea96a767be4c8b8d
Janmey <jshukla@zscaler.com>
2024-04-19 00:05:46 +0530

    Problem 463 - rec solution done

---
+++ leetcode/hard/42-trap-rain/new-sol.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+   public:
+    int trap(vector<int>& height) {
+        // will hold indices
+        stack<int> st;
+
+        for (int i = 0; i < height.size(); i++) {
+            // while (st.empty)
+        }
+    }
+};
